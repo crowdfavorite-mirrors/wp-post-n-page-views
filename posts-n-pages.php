aa1ec9e57b96ba00679c7e8857dc1ad7b5e69cfa
Steven Mathias <steve@crowdfavorite.com>
2012-02-03 14:58:29 -0700

    Added version 1.0 from https://plugins.svn.wordpress.org/post-n-page-views/tags/1.0

---
+++ posts-n-pages.php
@@ -0,0 +1,100 @@
+<?php
+/*
+Plugin Name: Post 'n Page Views
+Plugin URI: http://www.designbyrono.com/posts-n-pages/
+Description:  Tracks the number of views in pages and posts, in the admin area. To show view counts in your post/page template, add this snippet of code: <code>&lt;?php print_page_views(get_the_ID()); ?&gt;</code>. Requires <em>JetPack by Wordpress.com</em>.
+Version: 1.0
+Author: Rono
+Author URI: http://www.designbyrono.com
+*/
+
+
+
+// ----- initialize
+
+add_action('admin_head',					'add_views_style');
+add_action('in_admin_header',				'wp_get_post_stats');
+add_action('wp_head',						'wp_get_post_stats');
+
+add_action('manage_posts_custom_column',	'show_views_in_row',10,2);
+add_action('manage_pages_custom_column',	'show_views_in_row',10,2);
+
+add_filter('manage_pages_columns',			'show_views_in_header');
+add_filter('manage_posts_columns',			'show_views_in_header');
+
+
+
+// ----- add_views_style()
+
+function add_views_style(){
+
+	echo
+	'<style type="text/css">
+	.column-views {
+		width: 60px;
+		text-align: right !important;
+	}
+	</style>';
+}
+
+
+
+// ----- show_views_in_header()
+
+function show_views_in_header($columns){
+	$columns['views'] = __('Views');
+	return $columns;
+}
+
+
+
+// ----- show_views_row()
+
+function show_views_in_row($column_name, $post_id){
+	if ($column_name != 'views') return;
+	$pv = wp_get_post_views($post_id);
+	echo $pv ? $pv : 0;
+}
+
+
+
+// ----- main()
+
+if (!function_exists('print_page_views')):
+
+	function print_page_views($post_id) {
+		$views = wp_get_post_views($post_id);
+		$views = $views ? $views : 0;
+		echo number_format_i18n($views) . ' views';
+	}
+
+endif;
+
+
+
+// ----- wp_get_post_stats()
+
+function wp_get_post_stats() {
+	$post_stats = null;
+
+	if (function_exists('stats_get_csv')) {
+		global $post_stats;
+		$post_stats = stats_get_csv('postviews', '&days=-1&limit=-1&summarize');
+	}
+}
+
+
+
+// ----- wp_get_post_views()
+
+function wp_get_post_views($post_id) {
+	global $post_stats;
+
+	foreach ($post_stats as $p) {
+		if ($p['post_id'] == $post_id) {
+			return $p['views'];
+		}
+	}
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,64 @@
+﻿=== Post 'n Page Views ===
+Contributors: designbyrono
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=N5TKVJK6MVV46
+Tags: admin, post, page, stats, views, easy stats
+Requires at least: 2.5
+Tested up to: 3.1
+Stable tag: 1.0
+
+Reports the number of views for pages and posts, in the admin lists and can be used in the front end.
+
+
+== Description ==
+
+This very simple plugin keeps a count of your post/page views:
+
+* In the admin post and page tabs, it adds a neat column that tracks the number of views.
+
+* In the front end, you can display page or post view by adding the following code in your templates: `<?php print_page_views(get_the_ID()); ?>`. The default output will be something like: <strong>235 views</strong>
+
+
+This plugin requires the new JetPack by Wordpress.com or the old Wordpress.com Stats plugins.
+
+For a live demo, visit: http://www.AbsoluteFiction.com
+
+Inspired by 'Easy Stats 1.0', by WPCEO.
+
+
+== Installation ==
+
+1. Upload `post-n-page-views` to the `/wp-content/plugins/` directory
+
+2. Activate `Post 'n Page Views` through the 'Plugins' menu in WordPress
+
+3. Place `<?php print_page_views(get_the_ID()); ?>` in your templates
+
+
+== Screenshots ==
+
+1. Administration panel post list
+
+1. Administration panel page list
+
+2. User aera posts page
+
+
+== Frequently Asked Questions ==
+
+= Can I hide the stats from users that are not logged in? =
+
+Yes. In your templates, simply use the following code instead:
+`<?php if ($user_ID): ?> ... <?php print_page_views(get_the_ID()); ?> ... <?php endif; ?>`
+Replace the '...' with whatever other HTML you need to keep your design consistent.
+
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
+
+
+== Upgrade Notice ==
+
+= 1.0 =
+* None.
